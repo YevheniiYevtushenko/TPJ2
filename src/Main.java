f54146f73c2717b250a0e8659236db0571bcb343
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-09-26 03:01:12 +0300

    Add Date Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        System.out.println("26.09.2022");
+        System.out.println("25.09.2022");
         // Доданий з веб-браузера коментар
     }
 }
